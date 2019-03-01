11445137b7b83991b04cf19ba6f555fa2189cc47
bondarenkojek <bondarenkojek@gmail.com>
2019-03-01 23:40:36 +0200

    first comit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,32 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    public String getBotUsername() {
+        return "my_new_youtube_bot";
+    }
+
+    public String getBotToken() {
+        return "720463305:AAETvlYKjL1GWhZ_EADVLahcwzhVk0RLIhI";
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
