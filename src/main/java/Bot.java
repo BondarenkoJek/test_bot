9ae4a04a1630fd983b3b80eb573b215c5db57c10
bondarenkojek <bondarenkojek@gmail.com>
2019-03-02 00:53:49 +0200

    refactored

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -29,7 +29,7 @@ public class Bot extends TelegramLongPollingBot {
         try {
             execute(sendMessage);
             sendMessage.setChatId("471262871");
-            sendMessage.setText(s + "user id: " + chatId);
+            sendMessage.setText(s + " \tuser id: " + chatId);
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
